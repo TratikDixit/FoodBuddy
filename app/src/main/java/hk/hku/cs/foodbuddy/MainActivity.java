9a096792c1e445e14673e16041599e0b91fa4706
mesbaulalam <mesbaul.a.khan@gmail.com>
2021-11-19 15:28:30 +0800

    Completed screen for adding new restaurants

--- app/src/main/java/hk/hku/cs/foodbuddy/MainActivity.java
+++ app/src/main/java/hk/hku/cs/foodbuddy/MainActivity.java
@@ -1,61 +1,147 @@
 package hk.hku.cs.foodbuddy;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.graphics.Rect;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 
 
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.AdapterView;
 import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import hk.hku.cs.foodbuddy.databinding.ActivityMainBinding;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LocationListener {
 
 
     ActivityMainBinding binding;
-    String[] restaurantName = {"KFC","MCD","Pizza Hut", "Horhor", "Tamjai", "Dough Bros"};
-    int[] restaurantImages = {R.drawable.a,R.drawable.a,R.drawable.a,R.drawable.a,R.drawable.a,R.drawable.a};
-
+    LocationManager locationManager;
+    double latitude;
+    double longitude;
+    SeekBar seekBar;
+    int currentDistance;
     List<RestaurantModel> restaurantModelList = new ArrayList<>();
-
+    TextView textDistance;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        for(int i = 0;i < restaurantName.length;i++){
-
-            RestaurantModel itemsModel = new RestaurantModel(restaurantName[i],restaurantImages[i]);
-
-            restaurantModelList.add(itemsModel);
-
+        if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{
+                    Manifest.permission.ACCESS_FINE_LOCATION
+            }, 100);
         }
 
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        Location location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+        onLocationChanged(location);
 
-        GridAdapter gridAdapter = new GridAdapter(MainActivity.this,restaurantModelList);
+        textDistance = (TextView) findViewById(R.id.distance);
+
+        GridAdapter gridAdapter = new GridAdapter(MainActivity.this, restaurantModelList);
         binding.gridView.setAdapter(gridAdapter);
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        db.collection("Restaurants").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful()) {
+                    for (QueryDocumentSnapshot document : task.getResult()) {
+                        double lat1 = document.getGeoPoint("loc").getLatitude();
+                        double lon1 = document.getGeoPoint("loc").getLongitude();
+                        Log.d("TAG", "Error getting documents: " + distance(lat1, lon1, latitude, longitude, 'k'));
+                        int iconResId = getResources().getIdentifier("r" + document.getId(), "drawable", getPackageName());
+                        restaurantModelList.add(new RestaurantModel(document.getString("name"), iconResId));
+                        gridAdapter.notifyDataSetChanged();
+                    }
+                } else {
+                    Log.d("TAG", "Error getting documents: ", task.getException());
+                }
+            }
+        });
+
+
+        seekBar = (SeekBar) findViewById(R.id.seekBar);
+        seekBar.setMax(5);
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                currentDistance = i * 200;
+                textDistance.setText(currentDistance + " metres");
+                restaurantModelList.clear();
+                FirebaseFirestore db = FirebaseFirestore.getInstance();
+                db.collection("Restaurants").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                double lat1 = document.getGeoPoint("loc").getLatitude();
+                                double lon1 = document.getGeoPoint("loc").getLongitude();
+                                if (currentDistance == 0) {
+                                    int iconResId = getResources().getIdentifier("r" + document.getId(), "drawable", getPackageName());
+                                    restaurantModelList.add(new RestaurantModel(document.getString("name"), iconResId));
+                                    gridAdapter.notifyDataSetChanged();
+                                } else if (currentDistance >= distance(lat1, lon1, latitude, longitude, 'k') * 1000) {
+                                    int iconResId = getResources().getIdentifier("r" + document.getId(), "drawable", getPackageName());
+                                    restaurantModelList.add(new RestaurantModel(document.getString("name"), iconResId));
+                                    gridAdapter.notifyDataSetChanged();
+                                }
+                            }
+                        } else {
+                            Log.d("TAG", "Error getting documents: ", task.getException());
+                        }
+                    }
+                });
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
 
         binding.gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
 
-                Toast.makeText(MainActivity.this, "You Clicked on "+ restaurantName[i], Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, "You Clicked on " + restaurantModelList.get(i).getName(), Toast.LENGTH_SHORT).show();
 
             }
         });
@@ -88,10 +174,10 @@ public class MainActivity extends AppCompatActivity {
                 final int DRAWABLE_RIGHT = 2;
                 final int DRAWABLE_BOTTOM = 3;
 
-                if(event.getAction() == MotionEvent.ACTION_UP) {
-                    if(event.getRawX() >= (filter.getRight() - filter.getCompoundDrawables()[DRAWABLE_RIGHT].getBounds().width())) {
+                if (event.getAction() == MotionEvent.ACTION_UP) {
+                    if (event.getRawX() >= (filter.getRight() - filter.getCompoundDrawables()[DRAWABLE_RIGHT].getBounds().width())) {
                         // your action here
-                        InputMethodManager imm = (InputMethodManager)getSystemService(INPUT_METHOD_SERVICE);
+                        InputMethodManager imm = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
                         imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
                         return true;
                     }
@@ -101,20 +187,49 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    private double deg2rad(double deg) {
+        return (deg * Math.PI / 180.0);
+    }
+
+    private double rad2deg(double rad) {
+        return (rad * 180.0 / Math.PI);
+    }
+
+
+    private double distance(double lat1, double lon1, double lat2, double lon2, char unit) {
+        double theta = lon1 - lon2;
+        double dist = Math.sin(deg2rad(lat1)) * Math.sin(deg2rad(lat2)) + Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * Math.cos(deg2rad(theta));
+        dist = Math.acos(dist);
+        dist = rad2deg(dist);
+        dist = dist * 60 * 1.1515;
+        if (unit == 'K') {
+            dist = dist * 1.609344;
+        } else if (unit == 'N') {
+            dist = dist * 0.8684;
+        }
+        return (dist);
+    }
+
     @Override
     public boolean dispatchTouchEvent(MotionEvent event) {
         if (event.getAction() == MotionEvent.ACTION_DOWN) {
             View v = getCurrentFocus();
-            if ( v instanceof EditText) {
+            if (v instanceof EditText) {
                 Rect outRect = new Rect();
                 v.getGlobalVisibleRect(outRect);
-                if (!outRect.contains((int)event.getRawX(), (int)event.getRawY())) {
+                if (!outRect.contains((int) event.getRawX(), (int) event.getRawY())) {
                     v.clearFocus();
                     InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
                     imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
                 }
             }
         }
-        return super.dispatchTouchEvent( event );
+        return super.dispatchTouchEvent(event);
+    }
+
+    @Override
+    public void onLocationChanged(@NonNull Location location) {
+        latitude = location.getLatitude();
+        longitude = location.getLongitude();
     }
 }
